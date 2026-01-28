6ae43d6c68b61b6ca2ef4e124c5a4039620f4714
Colemumf <Cole.Mumford1@marist.edu>
2026-01-28 17:21:26 -0500

    Add files via upload
    homework 1

---
+++ hwone.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class hwone {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How old are you?");
+        String age = sc.nextLine();
+        System.out.println("You are " + age + " years old!");
+        int num1=10;
+        int num2=37;
+        System.out.println(num1+num2);
+
+        //I learned how to assign variables to certain things based off of what they are.(Words, numbers booleans)
+        //I also learned how to take input from the user by importing and using scanner and 
+        //I learned concatenation
+    }
+}
+    
+
